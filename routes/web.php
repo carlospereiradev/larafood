077c550007a5dcefa67cb73b3a22755a23b5324e
carlospereiradev <carlos.187.pereira@gmail.com>
2022-11-04 22:39:11 -0300

    Cadastrar Novo Plano

--- routes/web.php
+++ routes/web.php
@@ -3,7 +3,8 @@
 use App\Http\Controllers\Admin\PlanController;
 use Illuminate\Support\Facades\Route;
 
-
+Route::post('admin/plans', [PlanController::class, 'store'])->name('plans.store');
+Route::get('admin/plans/create', [PlanController::class, 'create'])->name('plans.create');
 Route::get('admin/plans', [PlanController::class, 'index'])->name('plans.index');
 
 Route::get('/', function () {
